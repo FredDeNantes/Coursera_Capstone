83cea922c9078ce2571fa70f6b3ecc8a94c724ca
FredDeNantes <63731951+FredDeNantes@users.noreply.github.com>
2020-04-16 18:22:22 +0200

    ajout debut capstone python data science

---
+++ Capstone20200416.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#
+# Python Data science cap stone - Fred Pallu april 2020
+#
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
+
+print('--------------- end of prog ---------------')
+
+
+# In[ ]:
+
+
+
+
